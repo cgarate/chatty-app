162c2f44bc45549494d3ae147dade1225e52e797
Carlos Garate <carlos.m.garate@gmail.com>
2017-05-04 17:18:20 -0400

    Added styles to navbar.

--- src/UsersOnline.jsx
+++ src/UsersOnline.jsx
@@ -4,7 +4,7 @@ class UsersOnline extends Component {
   render() {
     console.log("Rendering <UsersOnline/>")
     return (
-      <div>
+      <div className="usersOnline">
         <span>{this.props.users} Users online</span>
       </div>
     )
